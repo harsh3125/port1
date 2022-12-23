9947769d8fee15f4552d4846fb7cb4741c28cc02
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 13:31:31 +0600

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Teton Private Limited',
-        jobtitle: 'Software Engineer I',
-        startYear: 'Jan 2022',
+        company: 'Sapiens',
+        jobtitle: 'Software Devloper',
+        startYear: 'Jun 2023',
         endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Fiverr(freelance)',
+        company: 'Virtusa',
         jobtitle: 'Full-Stack Developer',
-        startYear: 'Jun 2021',
-        endYear: 'Jan 2022'
+        startYear: 'Jun 2022',
+        endYear: 'April 2023'
     },
     {
         id: 3,
-        company: 'Learning New Technology For Everyday',
-        jobtitle: 'Self Learning',
-        startYear: '2018',
-        endYear: 'Present'
+        company: 'Sparks Foundation',
+        jobtitle: 'Android Devloper',
+        startYear: '2022',
+        endYear: '2023'
     },
 ]
