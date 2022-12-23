adde501d44f311fc8b525180681cab9ffac5cdf4
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 13:58:07 +0600

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -63,6 +63,24 @@ export const projectsData = [
         demo: 'https://github.com/harsh3125/social_app',
         image: one
     },
+    {
+        id: 7,
+        projectName: 'Stock Market App',
+        projectDesc: 'A simple stock market API app',
+        tags: ['React', 'Redux', 'Bootstrap'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: two
+    },
+    {
+        id: 8,
+        projectName: 'Car Pooling System',
+        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+        tags: ['Flutter', 'React'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: three
+    },
 ]
 
 
