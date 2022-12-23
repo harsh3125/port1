23b8fd78f5999f658067228449fc77bc1f07f6a3
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 16:42:19 +0600

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,8 +4,7 @@ import four from '../assets/svg/projects/four.svg'
 import one from '../assets/svg/projects/one.svg'
 import seven from '../assets/svg/projects/seven.svg'
 import six from '../assets/svg/projects/six.svg'
-import three from '../assets/svg/projects/three.svg'
-import two from '../assets/svg/projects/two.svg'
+
 
 
 export const projectsData = [
