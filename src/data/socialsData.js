036070cde839ed20fc0607b257d363883841a003
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 13:08:27 +0600

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,9 +1,9 @@
 export const socialsData = {
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    medium: 'https://medium.com/@abusaid-bd',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    mail: 'abusaid7388@gmail.com',
+    github: 'https://github.com/harsh3125',
+    facebook: 'https://www.facebook.com/profile.php?id=100007111621882',
+    linkedIn: 'https://www.linkedin.com/in/harsh-agrawal-735b6418b/',
+    twitter: 'https://twitter.com/harsh',
+    medium: 'https://medium.com/harsh',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/harsh',
+    mail: 'harsh2531agrawal@gmail.com',
 }
