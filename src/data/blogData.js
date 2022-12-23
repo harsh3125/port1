b932bb42dc0a5a429a3e04b99b84dbff77b87f1c
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 16:15:48 +0600

    Update blogData.js

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,42 +1,43 @@
 export const blogData = [
     {
         id: 1,
-        title: 'SCRAPING BILLBOARD AND IMDB USING PYTHON SELENIUM',
-        description: 'Here we will use Selenium to navigate between web pages, and try to scrape data from them.',
-        date: 'Aug 7, 2020',
-        image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fw7.pngwing.com%2Fpngs%2F118%2F895%2Fpng-transparent-hackerrank-logos-and-brands-line-filled-icon.png&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dhackerrank&tbnid=NSuXXjrEKIOxMM&vet=12ahUKEwju7c7rlY_8AhU5odgFHTeICcYQMygFegUIARDSAQ..i&docid=7m3gcmU7n331WM&w=920&h=920&q=hackerrank%20logo&ved=2ahUKEwju7c7rlY_8AhU5odgFHTeICcYQMygFegUIARDSAQ'
+        title: 'FreeCodeCamp',
+        description: 'Website Devlopment',
+        date: 'May 31, 2020',
+        image: 'https://i.ibb.co/gwrQFD4/Whats-App-Image-2022-12-23-at-3-49-58-PM.jpg',
+        url: 'https://www.freecodecamp.org/certification/fcca33ec404-6c95-4443-a1ce-86810a2f1a51/responsive-web-design'
     },
     {
         id: 2,
-        title: 'CHECK WEATHER FROM TERMINAL USING WTTR.IN',
-        description: 'wttr.in is a console-oriented weather forecast service that supports various information representation methods like ANSI-sequences for console HTTP clients, HTML, or PNG.',
+        title: 'Prograte',
+        description:'Introduction to Python',
         date: 'Aug 11, 2020',
-        image: 'https://1.bp.blogspot.com/-OW7jX57tea4/XvnGxuEOslI/AAAAAAAABW0/R8lVT1AXDSwnvE0EGA9Ra49-LDm1ACwDgCK4BGAsYHg/s1216/wttr1.png',
-        url: ''
+        image: 'https://i.ibb.co/p27D3zP/Whats-App-Image-2022-12-23-at-3-49-19-PM.jpg',
+        url: 'https://progate.com/courses/python'
     },
     {
         id: 3,
-        title: 'BEING ANONYMOUS: A BEGINNERS GUIDE',
-        description: 'Here are some basic ways to lay off the Radar',
+        title: 'Codechef',
+        description: 'Winner Of Codeyssey Competiition',
         date: 'Aug 14, 2020',
-        image: 'https://1.bp.blogspot.com/-OW7jX57tea4/XvnGxuEOslI/AAAAAAAABW0/R8lVT1AXDSwnvE0EGA9Ra49-LDm1ACwDgCK4BGAsYHg/s1216/wttr1.png',
-        url: ''
+        image: 'https://i.ibb.co/vm2DVQ6/Whats-App-Image-2022-12-23-at-3-50-14-PM.jpg',
+        url: 'https://www.codechef.com/users/harsh3125'
     },
     {
         id: 4,
-        title: 'CHECK INTERNET SPEED FROM THE COMMAND LINE',
-        description: 'Want to check your internet speed from the terminal? Here\'s the perfect way to the that.',
-        date: 'Sep 17, 2020',
-        image: 'https://1.bp.blogspot.com/-XTVMsZP3HaM/Xxpl394txlI/AAAAAAAABws/zovXWiuNGg4TzBwYYVFfJ1ixDb3JfVokgCNcBGAsYHQ/w400-h195/speedtest.png',
-        url: ''
+        title: 'Virtusa',
+        description: 'Got Internship By Wining the Hackathon',
+        date: 'Mar 1, 2022',
+        image: 'https://i.ibb.co/4jd82jj/Whats-App-Image-2022-12-23-at-3-48-59-PM.jpg',
+        url: 'https://drive.google.com/file/d/1I5fYcZ2iZSAinxtv_DtjPj54zloYrdVE/view?usp=sharing'
     },
     {
         id: 5,
-        title: 'GET MICHAEL\'S OR TREVOR\'S VEHICLES FULLY UPGRADED IN GTA V FOR FREE',
-        description: 'In case you haven\'t heard of this game, GTA stands for \'Grand Theft Auto\'. It is an open-world game published by Rockstar Games.',
-        date: 'Oct 1, 2020',
-        image: 'https://1.bp.blogspot.com/-jhdtUBIRD_s/XxLFKQuRz9I/AAAAAAAABsM/mClfbfDFKvsUwA7Wyi25Lzu3DEmL2lxrACNcBGAsYHQ/s1919/gta.jpg',
-        url: ''
+        title: 'Hackerrank',
+        description: 'Intermediate Problem Solver',
+        date: 'May 1, 2020',
+        image: 'https://i.ibb.co/Fn3YrR8/Whats-App-Image-2022-12-23-at-3-50-31-PM.jpg',
+        url: 'https://www.hackerrank.com/certificates/c1bdd49d76bc'
     },
 ]
 
