725e3bbd17271f7ff19c181f82f3fc1cac62a5ae
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 13:47:44 +0600

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,75 +11,57 @@ import two from '../assets/svg/projects/two.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Ayla Networks',
-        projectDesc: 'This project about Simplifying the Development of Smart Home & IoT Solutions.',
-        tags: ['React', 'SAAS', 'React Bootstrap'],
+        projectName: 'Safar',
+        projectDesc: 'This project about making the travel easier.',
+        tags: ['Android', 'Firebase', 'Android Studio'],
         code: '',
-        demo: 'https://www.aylanetworks.com/',
+        demo: 'https://github.com/harsh3125/Safar_1.git',
         image: six
     },
     {
         id: 2,
-        projectName: 'Jewelry Niche',
-        projectDesc: 'This project is a Jewelry shop, Customer can be buy product and manage them and make payment.',
-        tags: ['React', 'Express', 'Material Ui'],
-        code: 'https://github.com/said7388/jewelry-niche',
-        demo: 'https://pandora-web.netlify.app/',
+        projectName: 'Slot Booking',
+        projectDesc: 'This project is used to make slot booking for students living in hostel.',
+        tags: ['React', 'Express', 'Andorid'],
+        code: 'https://github.com/harsh3125/washing-machine',
+        demo: 'https://github.com/harsh3125/washing-machine',
         image: seven
     },
     {
         id: 3,
-        projectName: 'Travel Agency',
-        projectDesc: 'This project about travel agency management.',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/said7388/Travel-Agency-client',
-        demo: 'https://travel-agency-fc58b.web.app/',
+        projectName: 'Rock Paper Scissor',
+        projectDesc: 'This project about making simple game using python',
+        tags: ['Python', 'VSCODE', 'Design'],
+        code: 'https://github.com/harsh3125/start-of-python',
+        demo: 'https://github.com/harsh3125/start-of-python',
         image: eight
     },
     {
         id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Portfolio',
+        projectDesc: 'This project involves the development of HTML CSS REACT JAVASCRIPT TO BUILD MY PORTFOLIO',
+        tags: ['React', 'JavaScript','CSS'],
+        code: 'https://github.com/harsh3125/port1',
+        demo: 'https://github.com/harsh3125/port1',
         image: four
     },
     {
         id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
+        projectName: 'Instagram Backend Api',
+        projectDesc: 'This api is devloped using golang as mentioned in the task',
         tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://github.com/harsh3125/Instagram-backend-api',
+        demo: 'https://github.com/harsh3125/Instagram-backend-api',
         image: five
     },
     {
         id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Social App',
+        projectDesc: 'Social App',
+        tags: ['Android'],
+        code: 'https://github.com/harsh3125/social_app',
+        demo: 'https://github.com/harsh3125/social_app',
         image: one
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: two
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
     },
 ]
 
