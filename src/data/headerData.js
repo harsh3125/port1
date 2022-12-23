7ff0c8dc17713eedd8c93bcc9526025a70452438
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 13:24:27 +0600

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ export const headerData = {
     name: 'Harsh Agrawal',
     title: "Hello! I am",
     desciption: "Hello! I am Harsh Agrawal, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://ibb.co/ZBcNxd9',
+    image: 'https://i.ibb.co/fkrQHMJ/Whats-App-Image-2022-12-23-at-1-17-11-PM.jpg',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: 'https://drive.google.com/file/d/11zVDoDHWdlhMtN9LYNC_gTf0zW8C3kNM/view?usp=sharing'
 }
