3f791d0713df7a5e8015ebf3cafe872a3716e752
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2022-12-23 13:19:30 +0600

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 
 export const headerData = {
-    name: 'ABU SAID',
+    name: 'Harsh Agrawal',
     title: "Hello! I am",
-    desciption: "Hello! I am ABU SAID, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
+    desciption: "Hello! I am Harsh Agrawal, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+    image: 'https://ibb.co/ZBcNxd9',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/11zVDoDHWdlhMtN9LYNC_gTf0zW8C3kNM/view?usp=sharing'
 }
